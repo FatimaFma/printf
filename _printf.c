99b6116e1278c83c5e4168b38879d3bd223df135
SafaFarah <kangjisook13@gimail.com>
2023-03-27 04:47:30 -0700

    0. I'm not going anywhere.

--- _printf.c
+++ _printf.c
@@ -1,8 +1,54 @@
 #include "main.h"
-/** _printf - function that produces output according to a format
- * @format: string containing the characters and the specifiers
- * Return:  number of characters printed
+#include<stdarg.h>
 
+/**
+ *_printf - function that produces output according to a format
+ * @format: string containing the characters and the specifiers
+ *
+ * Return:  number of characters printed
+ */
 int _printf(const char *format, ...)
 {
+	unsigned int i;
+	char *s;
+	char c;
+	va_list arg;
+	unsigned int count = 0;
+
+	va_start(arg, format);
+
+	while (*format)
+	{
+		while (*format != '%')
+		{
+			putchar(*format);
+			format++;
+			count++;
+		}
+		format++;
+		switch (*format)
+		{
+			case 'c':
+				i = va_arg(arg, int);
+				putchar(i);
+				count++;
+				break;
+			case 's':
+				s = va_arg(arg, char *);
+				while (*s != '\0')
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+				break;
+			case '%':
+				putchar(37);
+				count++;
+				break;
+		}
+		format++;
+	}
+	va_end(arg);
+	return (count);
 }
