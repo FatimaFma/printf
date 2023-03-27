83028a148ebb5399b067657b10317090e2a97496
Fatima Mustafa <toomyox@gmail.com>
2023-03-27 01:50:45 -0700

    creating _printf.c file

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/** _printf - function that produces output according to a format
+ * @format: string containing the characters and the specifiers
+ * Return:  number of characters printed
+
+int _printf(const char *format, ...)
+{
+}
