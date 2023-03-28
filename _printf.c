ed38475d769b86676473e23f293719284089427b
Fatima Mustafa <toomyox@gmail.com>
2023-03-28 15:02:21 -0700

    fixing

--- _printf.c
+++ _printf.c
@@ -4,7 +4,6 @@
 /**
  *_printf - function that produces output according to a format
  * @format: string containing the characters and the specifiers
- *
  * Return:  number of characters printed
  */
 int _printf(const char *format, ...)
